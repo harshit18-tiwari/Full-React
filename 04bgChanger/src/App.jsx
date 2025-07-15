f36665046c81a327fe8fa4e694a60d6a52caba4e
Harshit Tiwari <harshitoff18@gmail.com>
2025-07-15 18:58:48 +0530

    password-gen

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
 
   return (
     <div className="w-full h-screen duration-200"
-    style={{backgroundColor: color}}
+    style={{backgroundColor: color}} // mera state h yeh
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-          onClick={() => setColor("red")}
+          onClick={() => setColor("red")} // function k andhar ek aur function ko call kr rha h
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
           >Red</button>
